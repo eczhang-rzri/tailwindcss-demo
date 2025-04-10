17b2523b49386891c8cab054bd755387d4a0d652
eczhang-rzri <emily.zhang@utexas.edu>
2025-04-10 14:32:16 -0500

    update for lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -29,10 +27,10 @@ function App() {
 
   return (
     <>
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="profile-card">
+    <div className="flex justify-center items-center min-h-screen bg-black-100">
+      <div className="profile-card w-96 bg-cyan-100 shadow-lg rounded-lg p-12">
         {/* Profile image input */}
-        <div className="profile-image">
+        <div className="profile-image m-12 flex justify-center items-center hover:bg-stone-200 rounded-full transition duration-300 ease-in-out cursor-pointer">
           <input
             type="file"
             accept="image/*"
@@ -59,30 +57,30 @@ function App() {
           </div>
         </div>
           {/* Name input */}
-          <div className="mb-6">
+          <div className="mb-6 text-black border-black width-full p-2">
             <input
               type="text"
               placeholder="Your Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="name-input"
+              className="name-input sm:w-24 md:w-48 lg:w-64 focus:outline-none focus:border-blue-500 border-2 border-gray-500 rounded-lg p-2 w-full"
             />
           </div>
           {/* Bio textarea */}
-          <div className="mb-6">
+          <div className="mb-6 text-black border-black width-full p-2">
             <textarea
-              rows="3"
+              rows={3}
               placeholder="Write a short bio about yourself..."
               value={bio}
               onChange={(e) => setBio(e.target.value)}
-              className="bio-textarea"
+              className="bio-textarea sm:w-24 md:w-48 lg:w-64 focus:outline-none focus:border-blue-500 border-2 border-gray-500 rounded-lg p-2 w-full"
             ></textarea>
           </div>
         </div>
 
         {/* Save Changes Button */}
         <div className="save-button">
-          <button>
+          <button className="bg-blue-500 text-black-100 font-bold px-4 py-2 rounded-lg m-8 hover:bg-blue-700 transition duration-300 ease-in-out cursor-pointer">
             Save Changes
           </button>
         </div>
